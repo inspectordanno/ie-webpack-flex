97c09a955ceee119b59454289843534a41a73235
Dan Spector <danno2423@gmail.com>
2020-07-09 17:08:31 -0400

    Update Flex.js

--- src/components/Flex.js
+++ src/components/Flex.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Grid.module.scss';
+import styles from "./Grid.module.scss";
 
-const Grid = () => (
-  <div className={styles.flex}>
-    <div className={styles.square1}>One</div>
-    <div className={styles.square2}>Two</div>
-    <div className={styles.square3}>Three</div>
-    <div className={styles.square4}>Four</div>
-  </div>
-)
+function Flex() {
+  return (
+    <div className={styles.flex}>
+      <div className={styles.square1}>One</div>
+      <div className={styles.square2}>Two</div>
+      <div className={styles.square3}>Three</div>
+      <div className={styles.square4}>Four</div>
+    </div>
+  );
+}
 
-export default Grid;
+export default Flex;
